a137a96304a7388e44bff39d8b5b2c833dcd1f0a
hossainmahmood <hossain.it9@gmail.com>
2018-08-26 17:55:32 +0600

    Database work

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Org</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -77,7 +77,7 @@
                 </div>
             @endif
 
-            <div class="content">
+            <!--div class="content">
                 <div class="title m-b-md">
                     Laravel
                 </div>
@@ -89,7 +89,7 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
-            </div>
+            </div-->
         </div>
     </body>
 </html>
