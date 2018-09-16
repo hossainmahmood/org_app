a405a7a875430ddbf0c64ff709c8addf5604dc69
hossainmahmood <hossain.it9@gmail.com>
2018-09-16 17:07:23 +0600

    test

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+Route::resource('passports','PassportController');
